c3e36dc1d22fb53f3a7626586693113defdd54b2
MagmaX2 <chukwudoziembadugha@gmail.com>
2025-02-01 14:32:04 +0100

    learning is fun

---
+++ fceo/src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./home.css"
+
+export default function home
+() {
+  return (
+    <div className="container">
+        <main>
+        <h1>Hi, I'm</h1>
+        <h2>Choco</h2>
+        <p>A Frontend Developer</p>
+        <div className="button">Get in Touch</div>
+        </main>
+    </div>
+  )
+}
---
+++ react-project-ii/src/components/home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <header>
+            <h1> logo</h1>
+            <nav>
+                <ul>
+                    <li>menu</li>
+                    <li>gallery</li>
+                    <li>about us</li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default Home
