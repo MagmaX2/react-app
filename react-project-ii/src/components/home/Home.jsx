ed4f3bd6f400e8e5d419fd9863a6624b8e349e5e
MagmaX2 <chukwudoziembadugha@gmail.com>
2025-02-01 15:42:52 +0100

    added className for easy styling

--- react-project-ii/src/components/home/Home.jsx
+++ react-project-ii/src/components/home/Home.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 
-function Home() {
-  return (
-    <div>
-        <header>
-            <h1> logo</h1>
+function Home(){
+    return(
+        <>
+        <header className='header'>
+            <h1>logo</h1>
             <nav>
-                <ul>
+                <ul className='nav-list'>
                     <li>menu</li>
                     <li>gallery</li>
                     <li>about us</li>
                 </ul>
             </nav>
         </header>
-    </div>
-  )
+        <main>
+            <h1>something goes here</h1>
+            <h2>something else goes here</h2>
+        </main>
+    </>
+    )
 }
+
+
+
+
+
 
 export default Home
