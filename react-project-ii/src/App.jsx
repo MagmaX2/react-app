4dd633104bcaeb366ce90b0ce8017f6b22cf31eb
MagmaX2 <chukwudoziembadugha@gmail.com>
2025-02-02 11:39:25 +0100

    practicing composability

--- react-project-ii/src/App.jsx
+++ react-project-ii/src/App.jsx
@@ -1,14 +1,17 @@
 
 import './App.css'
-import Home from './components/home/Home'
+import Box from './components/home/Home'
 
 function App() {
   
 
   return (
-    <>
-     <Home/>
-    </>
+    <div className='body'>
+     <Box/>
+     <Box/>
+     <Box/>
+     <Box/>
+    </div>
   )
 }
 
--- react-project-ii/src/components/home/Home.jsx
+++ react-project-ii/src/components/home/Home.jsx
@@ -1,23 +1,37 @@
 import React from 'react'
+import './home.css'
 
-function Home(){
+function Box(){
     return(
-        <>
-        <header className='header'>
-            <h1>logo</h1>
-            <nav>
-                <ul className='nav-list'>
-                    <li>menu</li>
-                    <li>gallery</li>
-                    <li>about us</li>
-                </ul>
-            </nav>
-        </header>
-        <main>
-            <h1>something goes here</h1>
-            <h2>something else goes here</h2>
-        </main>
-    </>
+        // <div className='box'>
+        // <header className='header'>
+        //     <h3>logo</h3>
+        //     <nav className='nav'>
+        //         <ul className='nav-list'>
+        //             <li>menu</li>
+        //             <li>gallery</li>
+        //             <li>about us</li>
+        //         </ul>
+        //     </nav>
+        // </header>
+
+        <div className='box'>
+            <main className='main'>
+                <div className='text-box'>
+                <h3>First box</h3>
+                <p>name:Johnmary</p>
+                <p>phone:07053944685</p>
+                <p>email:chukwudoziembadugha@gmail.com</p>
+                <p>address:Eni-njoku hostel UNN</p>
+                </div>
+            </main>
+
+        <footer className='footer'>
+            <small>
+                @Johnmary All rights reserved
+            </small>
+        </footer>
+    </div>
     )
 }
 
@@ -26,4 +40,4 @@ function Home(){
 
 
 
-export default Home
+export default Box
